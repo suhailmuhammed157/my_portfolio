99f24a955e8d84c87de993b91273111232cedd70
suhailmuhammed157 <suhailmuhammed157@gmail.com>
2022-11-07 22:28:33 +0400

    added files

---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,11 @@
+import "./index.scss";
+import React from "react";
+import Sidebar from "../Sidebar";
+
+export default function Layout() {
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  );
+}
