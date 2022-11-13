26cd7b9c9991c74569d3b0aad4a610a9d7fe62ff
suhailmuhammed157 <suhailmuhammed157@gmail.com>
2022-11-13 19:37:43 +0400

    contact me WIP

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,10 +1,69 @@
+import { useEffect, useState } from "react";
 import Loader from "react-loaders";
+import AnimatedLetters from "../AnimatedLetters";
 import "./index.scss";
 
 export default function Contact() {
+  const [letterClass, setLetterClass] = useState("text-animate");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass("text-animate-hover");
+    }, 3000);
+  }, []);
   return (
     <>
-      <div className="container contact-page"></div>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              strArray={["C", "o", "n", "t", "a", "c", "t", " ", "m", "e"]}
+              idx={15}
+              letterClass={letterClass}
+            />
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+            mollitia expedita voluptatibus, dolores nam eligendi alias. Modi
+            praesentium alias perferendis?
+          </p>
+          <div className="contact-form">
+            <form>
+              <ul>
+                <li className="half">
+                  <input placeholder="Name" type="text" name="name" required />
+                </li>
+                <li className="half">
+                  <input
+                    placeholder="Email"
+                    type="email"
+                    name="email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+      </div>
       <Loader type="pacman" />
     </>
   );
