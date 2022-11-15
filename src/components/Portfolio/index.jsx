32c75ac2a5d8a8a0808383261c273b5d7c01ff52
Suhail SVI <suhail.m@svi.ae>
2022-11-15 12:45:30 +0400

    portfolio page complete

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -8,39 +8,54 @@ import portfolioData from "../../data/portfolio.json";
 import img from "../../assets/portfolio/chilled-grape/chilled-grape.png";
 
 export default function Portfolio() {
-  const [letterClass, setLetterClass] = useState("text-animate");
+    const [letterClass, setLetterClass] = useState("text-animate");
 
-  useEffect(() => {
-    setTimeout(() => {
-      setLetterClass("text-animate-hover");
-    }, 3000);
-  }, []);
+    useEffect(() => {
+        setTimeout(() => {
+            setLetterClass("text-animate-hover");
+        }, 3000);
+    }, []);
 
-  function renderPortfolio(portfolioArray) {
-    return portfolioArray.map((item, idx) => {
-      return (
-        <div className="image-box" key={idx}>
-          <img src={item.cover} alt="img" className="portfolio-image" />
+    function renderPortfolio(portfolioArray) {
+        return portfolioArray.map((item, idx) => {
+            return (
+                <>
+                    <div className="image-box" key={idx}>
+                        <img
+                            src={item.cover}
+                            alt="img"
+                            className="portfolio-image"
+                        />
+                        <div className="content">
+                            <p className="title">{item.title}</p>
+                            <h4 className="description">{item.description}</h4>
+                            <button
+                            className="btn"
+                            onClick={()=>window.open(item.url)}
+                            >view</button>
+                        </div>
+                    </div>
+                </>
+            );
+        });
+    }
+
+    return (
+        <div className="container portfolio-page">
+            <div className="text-zone">
+                <h1>
+                    <AnimatedLetters
+                        strArray={["P", "o", "r", "t", "f", "o", "l", "i", "o"]}
+                        letterClass={letterClass}
+                        idx={7}
+                    />
+                </h1>
+                <div className="images-container">
+                    {renderPortfolio(portfolioData.portfolio)}
+                </div>
+            </div>
+
+            <Loader type="pacman" />
         </div>
-      );
-    });
-  }
-
-  return (
-    <div className="container portfolio-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            strArray={["P", "o", "r", "t", "f", "o", "l", "i", "o"]}
-            letterClass={letterClass}
-            idx={7}
-          />
-        </h1>
-      </div>
-      {/* <div className="images-container">
-        {renderPortfolio(portfolioData.portfolio)}
-      </div> */}
-      <Loader type="pacman" />
-    </div>
-  );
+    );
 }
